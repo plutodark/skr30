13f0ec98d89e6434fca00557e7733376ae0c5a2d
paullaichampsys <plai@champ-sys.com>
2019-07-10 14:57:59 +0800

    test ENV value

--- src/gql/index.js
+++ src/gql/index.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
-
+console.log('REACT_APP_ENV', process.env.REACT_APP_ENV);
 const uri = process.env.REACT_APP_ENV === 'production' ? 'https://7qugq4cfl2.execute-api.ap-southeast-1.amazonaws.com/prod/graphql' : 'https://41i29nwo92.execute-api.ap-southeast-1.amazonaws.com/dev/graphql';
-
+console.log('uri', uri);
 const client = new ApolloClient({
  uri
 });
