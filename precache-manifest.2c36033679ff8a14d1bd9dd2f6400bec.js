c6a04072342b3f9a8d1121054936363260f96219
Paul Lai <plutodark@gmail.com>
2023-04-25 20:26:56 +0100

    Updates

---
+++ precache-manifest.2c36033679ff8a14d1bd9dd2f6400bec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834a16e6eb2382685f1a2540483a9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1ae27f4d72371b5bc2",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "0a0cb4754a452be43eb3",
+    "url": "/static/css/main.1cec4585.chunk.css"
+  },
+  {
+    "revision": "9f1ae27f4d72371b5bc2",
+    "url": "/static/js/2.02a45ee8.chunk.js"
+  },
+  {
+    "revision": "0a0cb4754a452be43eb3",
+    "url": "/static/js/main.19b8827f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2084bcac9c991fca53c7d9be534bc018",
+    "url": "/static/media/cam2Logo.2084bcac.jpg"
+  },
+  {
+    "revision": "5a1cec20b5ca83d48872ff24d4dfac33",
+    "url": "/static/media/home.5a1cec20.jpg"
+  },
+  {
+    "revision": "58eeec1c60f6eba6e88ab3c8866805f0",
+    "url": "/static/media/logoBlue.58eeec1c.png"
+  },
+  {
+    "revision": "55be3a09766119c2013f915bf90eb8e6",
+    "url": "/static/media/logoWhite.55be3a09.png"
+  }
+]);
